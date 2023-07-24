1066ecb463e52b864e66033b7d498aa2e60ca0eb
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-24 16:04:47 +0200

    Added class accessories

---
+++ models/Accessories.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+class Accessories extends Product
+{
+    private $material;
+    private $size;
+
+    public function __construct($title, $image, $price, $material, $size = 'ND')
+    {
+        parent::__construct($title, $image, $price);
+        $this->setMaterial($material);
+        $this->setSize($size);
+    }
+
+    // material
+    public function getMaterial()
+    {
+        return 'Materiale: ' . $this->material;
+    }
+
+    public function setMaterial($material)
+    {
+        if (!strlen($material)) return;
+        else $this->material = $material;
+    }
+
+    // size
+    public function getSize()
+    {
+        return 'Dimensioni: ' . $this->size;
+    }
+
+    public function setSize($size)
+    {
+        if (!strlen($size)) return 'ND';
+        else $this->size = $size;
+    }
+}
