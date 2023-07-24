a0bc22335b79898cac518b94b7b71835661bc9d3
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-24 16:15:48 +0200

    added class toy

---
+++ models/Toy.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+class Toy extends Product
+{
+    private $feature;
+    private $size;
+
+    public function __construct($title, $image, $price, $feature, $size = 'ND')
+    {
+        parent::__construct($title, $image, $price);
+        $this->setFeature($feature);
+        $this->setSize($size);
+    }
+
+    // feature
+    public function getFeature()
+    {
+        return 'Caratteristiche: ' . $this->feature;
+    }
+
+    public function setFeature($feature)
+    {
+        if (!strlen($feature)) return;
+        else $this->feature = $feature;
+    }
+
+    // size
+    public function getSize()
+    {
+        return 'Dimensioni: ' . $this->size;
+    }
+
+    public function setSize($size)
+    {
+        if (!strlen($size)) return 'ND';
+        else $this->size = $size;
+    }
+}
